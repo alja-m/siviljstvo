b3d96830eb3cc2e9a34a84af70595c9ac1cd33a2
saricaa7 <sara.hrastnik55@gmail.com>
2026-05-18 12:35:15 +0200

    naredla php

---
+++ projekt/data/www/nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg sticky-top">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+               <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Domov</a>
+                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="storitve.php">Storitve</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="galerija.php">Galerija</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
