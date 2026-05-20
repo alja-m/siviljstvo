749056bcce43e99df6fb49616118c6923c1d6095
saricaa7 <sara.hrastnik55@gmail.com>
2026-05-20 20:11:57 +0200

    .

---
+++ projekt/data/www/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer py-4">
+        <div class="container text-center">
+            <p class="mb-1">Šiviljstvo Novak, d. o. o.</p>
+            <p>Strma ulica 15, 2000 Maribor</p>
+            <div class="fs-3 mb-2">
+                <i class="bi bi-instagram me-3"></i>
+                <i class="bi bi-facebook"></i>
+            </div>
+            <p class="small">info@siviljstvonovak@.com | +386 31 567 890</p>
+        </div>
+</footer>
